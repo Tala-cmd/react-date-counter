b496a1493026b207fc7d0c428347ec3dc84ba929
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-22 02:06:53 +0300

    Date counter v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [count, setCount] = useState(0);
-  
-  function handleRemovingDay(){
-    const newDate = new Date(currentDate);
-    newDate.setDate(newDate.getDate() -1 )
-    setCurrentDate(newDate)
-    setCount((e) => e - 1)
-  }
-
-  function handleAddingDay(){
-    const newDate = new Date(currentDate);
-    newDate.setDate(newDate.getDate() +1 )
-    setCurrentDate(newDate)
-    setCount((e) => e + 1)
-  }
-
-  return (
-  <div>
-    <div className='hello'>
-      <button>-</button>
-      <h1>Step: </h1>
-      <button>+</button>
-    </div>
-
-    <div className='hello'>
-      <button onClick={handleRemovingDay}>-</button>
-      <h1>Count: {count}</h1>
-      <button onClick={handleAddingDay}>+</button>
-      <p></p>
-      
-    </div>
-    
-    {count > 0 
-      ? [count, ' ','days from today is', ' ', currentDate.toDateString()] 
-      : count === 0 
-      ? ['Today is', ' ', currentDate.toDateString()] 
-      : [Math.abs(count), ' ','days ago was', ' ', currentDate.toDateString()]
-    }
-  </div>
-  )
+export default function App() {
+  return <Counter />;
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+        <div className="step-and-counter">
+          <button onClick={()=> setStep(s=> s-1)}>-</button>
+          <h1>Step: {step}</h1>
+          <button onClick={()=> setStep(s=> s+1)}>+</button>
+        </div>
+
+        <div className="step-and-counter">
+          <button onClick={()=> setCount(c => c- step)}>-</button>
+          <h1>Count: {count}</h1>
+          <button onClick={()=> setCount(c => c+ step)}>+</button>
+        </div>
+        <p>
+          <span>
+              {count === 0
+                ? "Today is "
+                : count > 0
+                ? `${count} days from today is `
+                : `${Math.abs(count)} days ago was `}
+          </span>
+
+          <span>{date.toDateString()}</span>
+        </p>
+    </div>
+  );
+}
