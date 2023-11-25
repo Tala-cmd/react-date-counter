05ff9cbb773e22f3923ca71282b98b2a3d355c94
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-25 15:05:11 +0300

    Date counter v2

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,43 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function handleRangeChange(e){
+    setStep(Number(e.target.value))
+  }
+
+  function handleInput(e){
+    setCount(Number(e.target.value))
+  }
+
+  function handleResetButton(){
+    setCount(0)
+    setStep(1)
+  }
+
   return (
     <div>
-        <div className="step-and-counter">
-          <button onClick={()=> setStep(s=> s-1)}>-</button>
-          <h1>Step: {step}</h1>
-          <button onClick={()=> setStep(s=> s+1)}>+</button>
-        </div>
+      <div className="step-and-counter">
+        <input type="range" min='1' max='10' value={step} onChange={handleRangeChange}></input>
+        <span>Step: {step}</span>
+      </div>
 
-        <div className="step-and-counter">
-          <button onClick={()=> setCount(c => c- step)}>-</button>
-          <h1>Count: {count}</h1>
-          <button onClick={()=> setCount(c => c+ step)}>+</button>
-        </div>
-        <p>
-          <span>
-              {count === 0
-                ? "Today is "
-                : count > 0
-                ? `${count} days from today is `
-                : `${Math.abs(count)} days ago was `}
-          </span>
-
-          <span>{date.toDateString()}</span>
-        </p>
+      <div className="step-and-counter">
+        <button onClick={()=> setCount(c => c - step)}>-</button>
+        <input type="text" value={count} onChange={handleInput}></input>
+        <button onClick={()=> setCount(c => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {step !== 1 || count !==0 && 
+      (<button onClick={handleResetButton}>Reset</button>)}
     </div>
   );
 }
